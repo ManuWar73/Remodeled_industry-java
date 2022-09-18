85398a2d5d472bc1e372e1591683daefbb9f6a9d
ManuWar73 <manuelsf2009@gmail.ccom>
2022-09-18 19:16:41 +0200

    Ok

--- src/remodeledindustry/remodeledindustry.java
+++ src/remodeledindustry/remodeledindustry.java
@@ -1,6 +1,13 @@
 
 package remodeledindustry;
 import mindustry.mod.*;
+import mindustry.world.Block;
+import remodeledindustry.content.remBlocks;
 
 public class remodeledindustry extends Mod{
+
+@Override
+public void loadContent() {
+     Block puncher;
+}
 }
